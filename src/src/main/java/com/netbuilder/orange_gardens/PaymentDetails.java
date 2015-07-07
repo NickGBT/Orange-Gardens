864dc9e769bc01766085637cfa956b69f2f33c40
unknown <mwatson@PC-CZC45100JG.netbuilder.private>
2015-07-07 16:56:15 +0100

    PaymentDetails Class Added

---
+++ src/src/main/java/com/netbuilder/orange_gardens/PaymentDetails.java
@@ -0,0 +1,90 @@
+package src.main.java.com.netbuilder.orange_gardens;
+
+import java.util.Date;
+
+public class PaymentDetails {
+
+	public enum CardType { 
+		VISA, VISADEBIT,VISAELECTRON, MAESTRO, MASTERCARD;
+	}
+	CardType cardType;
+	int cardNumber;
+	String nameOnCard;
+	int securityNumber;
+	Date expiryDate;
+	int customerId;
+	
+	/**
+	 * @return the cardType
+	 */
+	public CardType getCardType() {
+		return cardType;
+	}
+	/**
+	 * @param cardType the cardType to set
+	 */
+	public void setCardType(CardType cardType) {
+		this.cardType = cardType;
+	}
+	/**
+	 * @return the cardNumber
+	 */
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	/**
+	 * @param cardNumber the cardNumber to set
+	 */
+	public void setCardNumber(int cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	/**
+	 * @return the nameOnCard
+	 */
+	public String getNameOnCard() {
+		return nameOnCard;
+	}
+	/**
+	 * @param nameOnCard the nameOnCard to set
+	 */
+	public void setNameOnCard(String nameOnCard) {
+		this.nameOnCard = nameOnCard;
+	}
+	/**
+	 * @return the securityNumber
+	 */
+	public int getSecurityNumber() {
+		return securityNumber;
+	}
+	/**
+	 * @param securityNumber the securityNumber to set
+	 */
+	public void setSecurityNumber(int securityNumber) {
+		this.securityNumber = securityNumber;
+	}
+	/**
+	 * @return the expiryDate
+	 */
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+	/**
+	 * @param expiryDate the expiryDate to set
+	 */
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	/**
+	 * @return the customerId
+	 */
+	public int getCustomerId() {
+		return customerId;
+	}
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+}
