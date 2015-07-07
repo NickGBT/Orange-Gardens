0fdba76e86d14c6657f2654506cd27c1653fca3d
unknown <ngilbert@PC-CZC4164ZVN.netbuilder.private>
2015-07-07 14:45:57 +0100

    added src

---
+++ src/main/java/com/netbuilder/jhn/App.java
@@ -0,0 +1,13 @@
+package com.netbuilder.jhn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
