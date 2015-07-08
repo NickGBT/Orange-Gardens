0390fd6494bdbab56fa092b07fbf167ee7cd9f2b
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-07-08 13:47:36 +0100

    DeliveryLine test cases

---
+++ src/test/java/com/netbuilder/orange_gardens/DeliveryLineTest.java
@@ -0,0 +1,38 @@
+package com.netbuilder.orange_gardens;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeliveryLineTest {
+	
+	DeliveryLine deliveryLine;
+	
+	private int productID = 45632;
+	private int deliveryID = 4264;
+	private int quantity = 250;
+
+	@Before
+	public void setUp() throws Exception {
+		deliveryLine = new DeliveryLine(productID, deliveryID, quantity);
+	}
+
+	@Test
+	public void testGetProductID() {
+		assertEquals(productID, deliveryLine.getProductID());;
+	}
+
+
+	@Test
+	public void testGetQuantity() {
+		assertEquals(quantity, deliveryLine.getQuantity());;
+	}
+
+
+	@Test
+	public void testGetDeliveryID() {
+		assertEquals(deliveryID, deliveryLine.getDeliveryID());;
+	}
+
+}
