67abdafa87f31e8d34d03db451bd1e2ba0be0a14
unknown <ngilbert@PC-CZC4164ZVN.netbuilder.private>
2015-07-08 09:25:52 +0100

    javadocs added to basketline and wishlistline

--- src/main/java/com/netbuilder/orange_gardens/BasketLine.java
+++ src/main/java/com/netbuilder/orange_gardens/BasketLine.java
@@ -1,5 +1,7 @@
 package com.netbuilder.orange_gardens;
-
+/**
+ * @return Created by Nick Gilbert
+ */
 public class BasketLine {
 	
 	private int quantity;
@@ -10,35 +12,51 @@ public class BasketLine {
 	public BasketLine(){
 		
 	}
-	
+	/**
+	 * @param set the quantity
+	 */
 	public void setQuantity(int quantity){
 		this.quantity = quantity;
 	}
-	
+	/**
+	 * @return the quantity
+	 */
 	public int getQuantity(){
 		return quantity;
 	}
-	
+	/**
+	 * @param set the porous ware availability
+	 */
 	public void setPorousware(boolean pware){
 		this.porousware = pware;
 	}
-	
+	/**
+	 * @return the porous ware availability
+	 */
 	public boolean getPorousware(){
 		return porousware;
 	}
-	
+	/**
+	 * @param set the orderID
+	 */
 	public void setOrderID(int orderID){
 		this.orderID = orderID;
 	}
-	
+	/**
+	 * @return the orderID
+	 */
 	public int getOrderID(){
 		return orderID;
 	}
-	
+	/**
+	 * @param set the productID
+	 */
 	public void setProductID(int productID){
 		this.productID = productID;
 	}
-	
+	/**
+	 * @return the productID
+	 */
 	public int getProductID(){
 		return productID;
 	}
--- src/main/java/com/netbuilder/orange_gardens/WishlistLine.java
+++ src/main/java/com/netbuilder/orange_gardens/WishlistLine.java
@@ -1,5 +1,7 @@
 package com.netbuilder.orange_gardens;
-
+/**
+ * Created by Nick Gilbert
+ */
 public class WishlistLine {
 	
 	private int quantity;
@@ -10,35 +12,51 @@ public class WishlistLine {
 	public WishlistLine(){
 		
 	}
-	
+	/**
+	 * @param set the quantity
+	 */
 	public void setQuantity(int quantity){
 		this.quantity = quantity;
 	}
-	
+	/**
+	 * @return the quantity
+	 */
 	public int getQuantity(){
 		return quantity;
 	}
-	
+	/**
+	 * @param set porous ware availability
+	 */
 	public void setPorousware(boolean pware){
 		this.porousware = pware;
 	}
-	
+	/**
+	 * @return if porous ware is available
+	 */
 	public boolean getPorousware(){
 		return porousware;
 	}
-	
+	/**
+	 * @param set the orderID
+	 */
 	public void setOrderID(int orderID){
 		this.orderID = orderID;
 	}
-	
+	/**
+	 * @return the orderID
+	 */
 	public int getOrderID(){
 		return orderID;
 	}
-	
+	/**
+	 * @param set the productID
+	 */
 	public void setProductID(int productID){
 		this.productID = productID;
 	}
-	
+	/**
+	 * @return the productID
+	 */
 	public int getProductID(){
 		return productID;
 	}
