16bf66dadc09dd069a796a15f745d970a2ce597a
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-08 11:57:57 +0100

    Initial tests for Address written.

--- src/test/java/com/netbuilder/orange_gardens/AddressTests.java
+++ src/test/java/com/netbuilder/orange_gardens/AddressTests.java
@@ -12,82 +12,66 @@ public class AddressTests extends TestCase {
 
 	private Address testAddress;
 	
+	String addressLabel = "Test Address";
+	
+	int customerId = 4672;
+	
+	String lineOne = "5 Orange Garden";
+	String lineTwo = "Ochre Business Park";
+	String lineThree = "Ambertown";
+	String city = "Orangedon";
+	String county = "Coppershire";
+	String postcode = "QW1 2ER";
+	
+	String country = "United Kingdom";
+	
 	protected void setUp() throws Exception {
 		super.setUp();
 		
-		testAddress = new Address();
+		testAddress = new Address(customerId, addressLabel, lineOne, lineTwo, lineThree, city, county, postcode, false);
 	}
 
 	public void testGetCustomerID() {
-		fail("Not yet implemented");
+		assertEquals(testAddress.getCustomerID(), customerId);
 	}
 
 	public void testGetAddressLabel() {
-		fail("Not yet implemented");
+		assertEquals(testAddress.getAddressLabel(), addressLabel);
 	}
-
+/*
 	public void testGetOrderID() {
 		fail("Not yet implemented");
 	}
-
+*/
 	public void testGetAddressLine1() {
 		
-		String lineOne = "5 Orange Garden";
-		
-		testAddress.setAddressLine1(lineOne);
-		
+	
 		assertEquals(testAddress.getAddressLine1(), lineOne);
 	}
 
 	public void testGetAddressLine2() {
 		
-		String lineTwo = "Ochre Business Park";
-		
-		testAddress.setAddressLine2(lineTwo);
-		
+
 		assertEquals(testAddress.getAddressLine2(), lineTwo);
 	}
 
 	public void testGetAddressLine3() {
-		
-		String lineThree = "Ambertown";
-		
-		testAddress.setAddressLine3(lineThree);
-		
+				
 		assertEquals(testAddress.getAddressLine3(), lineThree);
 	}
 
 	public void testGetCounty() {
-		
-		String county = "Coppershire";
-		
-		testAddress.setCounty(county);
-		
+				
 		assertEquals(testAddress.getCounty(), county);
 	}
 
 	public void testGetPostcode() {
-
-		String postcode = "QW1 2ER";
-		
-		testAddress.setPostcode(postcode);
 		
 		assertEquals(testAddress.getPostcode(), postcode);
 	}
 
-	public void testGetCountry() {
-
-		String country = "United Kingdom";
-		
-		testAddress.setCountry(country);
-		
-		assertEquals(testAddress.getCountry(), country);
-	}
-
 	public void testIsBillingAddress() {
-		
-		testAddress.setBillingAddress(false);
-		
+			
 		assertFalse(testAddress.isBillingAddress());
 	}
 
