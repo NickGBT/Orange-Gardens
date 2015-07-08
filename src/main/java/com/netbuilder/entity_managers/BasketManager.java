2d64a20c0b23e825e2c8d1de0a5937269ca52844
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-07-08 17:22:22 +0100

    Added Basket Manger class

---
+++ src/main/java/com/netbuilder/entity_managers/BasketManager.java
@@ -0,0 +1,21 @@
+package com.netbuilder.entity_managers;
+
+import java.util.ArrayList;
+import com.netbuilder.entities.Basket;
+
+public interface BasketManager {
+
+	//CREATE
+	public void persistBasket(Basket basket);
+	public void persistBasket(ArrayList<Basket> baskets);
+	
+	//READ
+	public Basket findByCustomerID(int customerID);
+	public Basket getBasket();
+	
+	//UPDATE
+	public void updateBasket(Basket basket);
+	
+	//DELETE
+	public void removeBasket(Basket basket);
+}
