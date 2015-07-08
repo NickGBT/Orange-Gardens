52dd47b73f4ff24266bed49c442e7dddca91f20f
JustinMabbutt <Justin.Mabbutt@netbuilder.com>
2015-07-08 17:32:24 +0100

    Entity Managers done - JM

---
+++ src/main/java/com/netbuilder/entity_managers/CustomerManager.java
@@ -0,0 +1,33 @@
+package com.netbuilder.entity_managers;
+
+import java.util.ArrayList;
+
+import com.netbuilder.entities.Customer;
+import com.netbuilder.entities.Delivery;
+
+/**
+ * 
+ * @author JustinMabbutt
+ *
+ */
+
+public interface CustomerManager
+{
+	//CREATE
+	public void persistCustomer(Customer customer);
+	public void persistCustomer(ArrayList<Customer> customers);
+	
+	//READ
+	public Customer findByUsername(String username);
+	public Customer findByEmail(String email);
+	public Customer findByFName(String fName);
+	public Customer findByLName(String lName);
+	public Customer findByCustomerID(int customerID);
+	public ArrayList<Customer> getCustomer();
+	
+	//UPDATE
+	public void updateCustomer(Customer customer);
+	
+	//DELETE
+	public void removeCustomer(Customer customer);
+}
