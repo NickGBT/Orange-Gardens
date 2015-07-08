cf9402a23d254da6c5e191833b894b2add515088
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-07-08 17:32:27 +0100

    Delivery Line manager

---
+++ src/main/java/com/netbuilder/entity_managers/DeliveryLineManager.java
@@ -0,0 +1,32 @@
+package com.netbuilder.entity_managers;
+
+import java.util.ArrayList;
+
+import com.netbuilder.entities.DeliveryLine;
+
+/**
+ * 
+ * @author llew
+ *
+ */
+
+public interface DeliveryLineManager {
+
+	// CREATE
+	public void persistDeliveryLine(DeliveryLine deliveryLine);
+
+	public void persistDeliveryLine(ArrayList<DeliveryLine> deliveryLines);
+
+	// READ
+	public DeliveryLine findByProductID(int postcode);
+	public DeliveryLine findByDeliveryID(int deliveryID);
+	public DeliveryLine findByQuantity(int quantity);
+
+	public ArrayList<DeliveryLine> getDeliveryLine();
+
+	// UPDATE
+	public void updateDeliveryLine(DeliveryLine deliveryLines);
+
+	// DELETE
+	public void removeDeliveryLine(DeliveryLine deliveryLines);
+}
