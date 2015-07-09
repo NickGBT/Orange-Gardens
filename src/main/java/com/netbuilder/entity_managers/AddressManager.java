78084a896dd648c71b6d7132a0896edbca74f2ac
JustinMabbutt <Justin.Mabbutt@netbuilder.com>
2015-07-09 09:13:40 +0100

    Weird stuff fixed - JM

--- src/main/java/com/netbuilder/entity_managers/AddressManager.java
+++ src/main/java/com/netbuilder/entity_managers/AddressManager.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 
 import com.netbuilder.entities.Address;
 
-<<<<<<< HEAD
-public interface AddressManager {
-	// CREATE
-=======
 /**
  * 
  * @author JustinMabbutt
@@ -17,19 +13,14 @@ public interface AddressManager {
 public interface AddressManager 
 {
 	//CREATE
->>>>>>> d61cb044c97864607127408e4e3e344c471d6a63
 	public void persistAddress(Address address);
 
 	public void persistAddresses(ArrayList<Address> addresses);
 
 	// READ
 	public Address findByPostcode(String postcode);
-<<<<<<< HEAD
-
-=======
 	public Address findByLabel(String label);
 	public Address findByCustomerID(int customerID);
->>>>>>> d61cb044c97864607127408e4e3e344c471d6a63
 	public ArrayList<Address> getAddresses();
 
 	// UPDATE
