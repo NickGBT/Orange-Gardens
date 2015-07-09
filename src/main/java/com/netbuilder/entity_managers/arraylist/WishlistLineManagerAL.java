22952d43e1b9dbdc4fb4e3854b9d09b68f307d8f
Nick <Nick.Gilbert@netbuilder.com>
2015-07-09 11:42:27 +0100

    changes to wishlistlinemanageral

--- src/main/java/com/netbuilder/entity_managers/arraylist/WishlistLineManagerAL.java
+++ src/main/java/com/netbuilder/entity_managers/arraylist/WishlistLineManagerAL.java
@@ -2,9 +2,20 @@ package com.netbuilder.entity_managers.arraylist;
 
 import java.util.ArrayList;
 
-import com.netbuilder.entities.Customer;
+import com.netbuilder.entities.WishlistLine;
 import com.netbuilder.entity_managers.interfaces.WishlistLineManager;
 
-public class WishlistLineManagerAL {
+public class WishlistLineManagerAL implements WishlistLineManager{
+	
+	private ArrayList <WishlistLine> wishlistLines = new ArrayList <WishlistLine>();
+	
+	public void persistWishlistLine(WishlistLine wishlistLine){
+		
+		wishlistLines.add(wishlistLine);
+	}
 
+	public void persistWishlistLines(ArrayList<WishlistLine> wishlistLines)
+	{
+		wishlistLines.addAll(wishlistLines);
+	}
 }
