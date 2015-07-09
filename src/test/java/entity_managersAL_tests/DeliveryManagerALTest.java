f7d83925a6b5ad1fde3c00a4e1acb033a11a9620
Nick <Nick.Gilbert@netbuilder.com>
2015-07-09 15:23:40 +0100

    added entity manager AL tests package, started deliverymanagerAL_tests

---
+++ src/test/java/entity_managersAL_tests/DeliveryManagerALTest.java
@@ -0,0 +1,49 @@
+package entity_managersAL_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DeliveryManagerALTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPersistDelivery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPersistDeliveries() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByDatePlaced() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByDeliveryID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDeliveries() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDelivery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveDelivery() {
+		fail("Not yet implemented");
+	}
+
+}
