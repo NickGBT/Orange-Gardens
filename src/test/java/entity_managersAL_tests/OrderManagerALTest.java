95ae3b80df9d6fed4b7f7a1100630f1b84286133
unknown <mwatson@PC-CZC45100JG.netbuilder.private>
2015-07-09 17:27:01 +0100

    OrderAL Test started

---
+++ src/test/java/entity_managersAL_tests/OrderManagerALTest.java
@@ -0,0 +1,94 @@
+package entity_managersAL_tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.netbuilder.entities.Order;
+import com.netbuilder.entity_managers.arraylist.OrderManagerAL;
+
+/**
+ * 
+ * @author mwatson
+ *
+ */
+
+public class OrderManagerALTest {
+
+	OrderManagerAL orderManagerAL;
+	
+	private ArrayList<Order> testArrayOrder, testArrayOrder2;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testPersistOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByOrderID() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByDatePlaced() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByDateDispatched() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByDateDelivered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByTwoDatesOrderPlaced() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByTwoDatesOrderDispatched() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByTwoDatesOrderDelivered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByCustomerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByEmployeeId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateWishList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateOrder() {
+		fail("Not yet implemented");
+	}
+
+}
