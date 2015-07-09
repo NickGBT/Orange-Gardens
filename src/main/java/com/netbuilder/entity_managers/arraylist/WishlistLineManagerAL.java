fb0bf9bd8af47c6042fc04dc7ac55c16813615a3
Nick <Nick.Gilbert@netbuilder.com>
2015-07-09 11:35:13 +0100

    added wishlistlinemanager

---
+++ src/main/java/com/netbuilder/entity_managers/arraylist/WishlistLineManagerAL.java
@@ -0,0 +1,10 @@
+package com.netbuilder.entity_managers.arraylist;
+
+import java.util.ArrayList;
+
+import com.netbuilder.entities.Customer;
+import com.netbuilder.entity_managers.interfaces.WishlistLineManager;
+
+public class WishlistLineManagerAL {
+
+}
