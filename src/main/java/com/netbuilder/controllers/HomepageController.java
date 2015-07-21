df9edfb4ea3e2e306e76af851fc227080df44a65
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-07-21 13:57:42 +0100

    Homepage controller

---
+++ src/main/java/com/netbuilder/controllers/HomepageController.java
@@ -0,0 +1,28 @@
+package com.netbuilder.controllers;
+
+import java.util.ArrayList;
+
+import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import com.netbuilder.entities.Product;
+import com.netbuilder.entity_managers.arraylist.ProductManagerAL;
+
+/*
+ * 
+ * @author llew
+ *
+ */
+
+ 
+public class HomepageController {
+	
+	@Inject
+	private ProductManagerAL pmal;
+	
+	public ArrayList<Product> getNewProducts(){
+		return pmal.findNewProducts();
+		}
+	
+	
+}
