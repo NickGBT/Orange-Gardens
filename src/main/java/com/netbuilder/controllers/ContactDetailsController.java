5d0b6b0d4eb30718b0ea0832df4843e6b8b28d68
Nick <Nick.Gilbert@netbuilder.com>
2015-07-21 11:17:42 +0100

    created controller classes

---
+++ src/main/java/com/netbuilder/controllers/ContactDetailsController.java
@@ -0,0 +1,10 @@
+package com.netbuilder.controllers;
+
+import javax.inject.Inject;
+
+import com.netbuilder.util.ContactUsDetails;
+
+public class ContactDetailsController {
+	@Inject
+	private ContactUsDetails contactDetails;
+}
