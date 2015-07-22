ad1a08255bebf5532e348deae0a8c3e8912f6560
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-07-22 17:02:46 +0100

    added author to PreviousOrderController

--- src/main/java/com/netbuilder/controllers/PreviousOrderController.java
+++ src/main/java/com/netbuilder/controllers/PreviousOrderController.java
@@ -7,10 +7,15 @@ import com.netbuilder.entity_managers.interfaces.OrderManager;
 import com.netbuilder.enums.OrderStatus;
 import com.netbuilder.util.CustomerUserId;
 
+/*
+ * 
+ * @author llew
+ *
+ */
+
 public class PreviousOrderController {
 	
 	private OrderManager orderMan;
-	private ArrayList<Order> prevOrders = new ArrayList<Order>();
 	private OrderStatus orderStatus = OrderStatus.placed;
 	
 	public ArrayList<Order> getPreviousOrders(){
