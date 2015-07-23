0dfc916859bef11632e9c00dd9ea9258fb8fcf93
unknown <mwatson@PC-CZC45100JG.netbuilder.private>
2015-07-23 15:49:54 +0100

    ProductController Completed V1.0

--- src/main/java/com/netbuilder/util/ProductDetails.java
+++ src/main/java/com/netbuilder/util/ProductDetails.java
@@ -3,8 +3,17 @@ package com.netbuilder.util;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
+import com.netbuilder.entities.LoginDetails;
+import com.netbuilder.entities.Order;
+import com.netbuilder.entities.OrderLine;
+import com.netbuilder.entities.PaymentDetails;
 import com.netbuilder.entities.Product;
+import com.netbuilder.entity_managers.arraylist.LoginDetailsManagerAL;
+import com.netbuilder.entity_managers.arraylist.OrderLineManagerAL;
+import com.netbuilder.entity_managers.arraylist.OrderManagerAL;
+import com.netbuilder.entity_managers.arraylist.PaymentDetailsManagerAL;
 import com.netbuilder.entity_managers.arraylist.ProductManagerAL;
+import com.netbuilder.entity_managers.interfaces.PaymentDetailsManager;
 import com.netbuilder.enums.OrderStatus;
 import com.netbuilder.enums.ProductCategory;
 
@@ -12,17 +21,23 @@ import com.netbuilder.enums.ProductCategory;
 @RequestScoped
 public class ProductDetails {
 
-	int quantity;
-	int productId;
-	int userId;
-	int orderId;
-	
-	ProductCategory category;
-	
-	OrderStatus status;
-	
+	private int quantity;
+	private int productId;
+	private int customerId;
+	private int orderId;
+	private ProductCategory category;
+	private OrderStatus status;
 	private ProductManagerAL productMan;
+	private PaymentDetails paymentD;
+	private PaymentDetailsManagerAL paymentMan;
+	private OrderManagerAL orderMan;
+	private OrderLineManagerAL orderLineMan;
 	private Product product;
+	private Order order;
+	private OrderLine orderLine;
+	private LoginDetailsManagerAL loginMan;
+	private LoginDetails loginD;
+	
 	
 	public Product getProductId() {
 		product = productMan.findByProductId(productId);
@@ -31,6 +46,124 @@ public class ProductDetails {
 	
 	public void setProductId(int productId) {
 		this.productId = productId;
+	}
+
+	/**
+	 * @return the quantity
+	 */
+	public int getQuantity() {
+		return quantity;
+	}
+
+	/**
+	 * @param quantity the quantity to set
+	 */
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the customerId
+	 */
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	/**
+	 * @param customerId the customerId to set
+	 */
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	/**
+	 * @return the orderId
+	 */
+	public int getOrderId() {
+		return orderId;
+	}
+
+	/**
+	 * @param orderId the orderId to set
+	 */
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	/**
+	 * @return the category
+	 */
+	public ProductCategory getCategory() {
+		return category;
+	}
+
+	/**
+	 * @param category the category to set
+	 */
+	public void setCategory(ProductCategory category) {
+		this.category = category;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public void addToWishlist(){
+		
+		if(orderMan.findByCustomerId(customerId) == null) {
+			order = new Order(loginD, OrderStatus.wishlist, paymentD);
+			orderLine = new OrderLine(order,product);
+			
+			orderMan.persistOrder(order);
+			orderLineMan.persistOrderLine(orderLine);
+			
+		} else { 
+			orderLine = new OrderLine(order,product);
+			orderLineMan.persistOrderLine(orderLine);
+		}
+				
+	}
+	
+	public void addToBasket() {
+		if(orderMan.findByCustomerId(customerId) == null) {
+			
+			order = new Order(loginD, OrderStatus.wishlist, paymentD);
+			orderLine = new OrderLine(order, product, quantity);
+			
+			orderMan.persistOrder(order);
+			orderLineMan.persistOrderLine(orderLine);
+		} else {
+			orderLine = new OrderLine(order,product);
+			orderLineMan.persistOrderLine(orderLine);
+		}	
+	}
+
+	/**
+	 * @return the paymentD
+	 */
+	public PaymentDetails getPaymentD() {
+		return paymentD;
+	}
+
+	/**
+	 * @param paymentD the paymentD to set
+	 */
+	public void setPaymentD(int customerId) {
+		this.paymentD =  paymentMan.findCustomerPaymentDetails(customerId); 
+	}
+
+	public void findLoginDetails(int customerId) {
+		this.loginD = loginMan.findByUserId(customerId);
 	}
 	
 	
