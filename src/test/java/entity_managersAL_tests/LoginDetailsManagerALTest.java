0ba03b5b833d127fc250af1adbda80cc4f85f42c
unknown <jtaylor@PC-CZC1209VTL.netbuilder.private>
2015-07-23 14:46:58 +0100

    Created loginDetailsManagerAL test -JT

--- src/test/java/entity_managersAL_tests/LoginDetailsManagerALTest.java
+++ src/test/java/entity_managersAL_tests/LoginDetailsManagerALTest.java
@@ -1,5 +1,7 @@
 package entity_managersAL_tests;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,8 +19,39 @@ import com.netbuilder.entity_managers.arraylist.LoginDetailsManagerAL;
 
 public class LoginDetailsManagerALTest 
 {
-	private LoginDetailsManagerAL LoginManager;
+	private LoginDetailsManagerAL loginDetailsManager;
 	private LoginDetails loginDetailsTest1, loginDetailsTest2;
+	private List<LoginDetails> testArrayLoginDetails;
+	byte[] testPassword1 = {1,2,3};
+	byte[] testSalt1 = {1,2,3};
+	byte[] testPassword2 = {2,3,4};
+	byte[] testSalt2 = {2,3,4};
 	
+	
+	@Before
+	public void setUp() throws Exception
+	{
+		loginDetailsManager = new LoginDetailsManagerAL();
+		testArrayLoginDetails = new ArrayList<LoginDetails>();
+		loginDetailsTest1 = new LoginDetails(123, "testUser1", testPassword1, testSalt1);
+		loginDetailsTest2 = new LoginDetails(456, "testUser2", testPassword2, testSalt2);
+	}
+	
+	@Test
+	public void testPersistLoginDetails()
+	{
+		loginDetailsManager.persistLoginDetails(loginDetailsTest1);
+		testArrayLoginDetails = loginDetailsManager.getAllLoginDetails();
+		assertEquals(testArrayLoginDetails.size(), 1);
+	}
+	
+	@Test 
+	void testFindByUserName()
+	{
+		testArrayLoginDetails.clear();
+		testArrayLoginDetails.add(loginDetailsTest1);
+		loginDetailsManager.persistLoginDetails(loginDetailsTest1);
+		assertEquals(loginDetailsManager.findByUsername("testUser1"), testArrayLoginDetails);
+	}
 	
 }
