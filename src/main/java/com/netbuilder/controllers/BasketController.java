6eb361188b20c9036580ad3e181d73b55f118715
Jordan Taylor <taylorcj@live.co.uk>
2015-07-27 14:20:42 +0100

    Created getBasket -JT

--- src/main/java/com/netbuilder/controllers/BasketController.java
+++ src/main/java/com/netbuilder/controllers/BasketController.java
@@ -1,9 +1,12 @@
 package com.netbuilder.controllers;
 
+import java.util.List;
+
 import javax.inject.Inject;
+
 import com.netbuilder.entity_managers.interfaces.OrderLineManager;
+import com.netbuilder.entities.OrderLine;
 import com.netbuilder.util.OrderDetails;
-import com.netbuilder.entities.Order;
 
 /**
  * 
@@ -14,16 +17,24 @@ import com.netbuilder.entities.Order;
 public class BasketController {
 	@Inject
 	private OrderLineManager orderLineManger;
-	private Order order;
+	private OrderDetails basketGetter;
+	public List<OrderLine> basket;
+	
+	public List<OrderLine> getBasket()
+	{
+		basket = basketGetter.getBasket();
+		return basket;	
+	}
 	
 	
 	public void updateProductQty() 
 	{ 
-		
+		//add into xhtml
 	}
 	
 	public void updatePorousware() 
 	{
+		//add into xhtml
 	}
 	
 	public void removeProduct() 
