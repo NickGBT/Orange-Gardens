64c52a189b36369ff2aa54bb268b58601116985e
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-27 17:25:52 +0100

    New login controller and interface for this on login.xhtml. -AN

---
+++ src/main/java/com/netbuilder/controllers/LoginController.java
@@ -0,0 +1,55 @@
+package com.netbuilder.controllers;
+
+import javax.ejb.Stateful;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import com.netbuilder.entity_managers.interfaces.LoginDetailsManager;
+
+/**
+ * 
+ * @author Alexander Neil
+ *
+ */
+
+@ManagedBean(name="login")
+@Stateful
+@SessionScoped
+public class LoginController {
+	
+	@Inject
+	private LoginDetailsManager ldm;
+	private String name;
+	private String password;
+	private int userId;
+
+	public String login(){
+		
+		userId = ldm.checkPassword(name, password);
+		
+		if(userId >= 0){
+			
+			return "account.xhtml";
+		}
+		else{
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Incorrect username/password combination!"));
+			return "login.xhtml";
+		}
+	}
+	
+	public String getName() { return name; }
+	public String getPassword() { return password; }
+	public void setName(String name) { this.name = name; }
+	public void setPassword(String password) { this.password = password; }
+
+	public void logout(){
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+	}
+	
+	public boolean isLoggedIn(){
+		return userId >= 0;
+	}
+}
