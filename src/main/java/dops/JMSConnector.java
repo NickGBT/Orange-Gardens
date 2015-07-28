fffad36fc3c6aa8976b61d188a2c556f0e0a6e34
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-28 17:27:51 +0100

    Empty methods and constructor for server JMS connector created. - AN

--- src/main/java/dops/JMSConnector.java
+++ src/main/java/dops/JMSConnector.java
@@ -5,15 +5,37 @@ import java.util.ArrayList;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 
+import com.netbuilder.entities.Order;
+
 /**
  * 
  * @author Alexander Neil
  *
  */
 
+
 public class JMSConnector implements MessageListener {
 
 	ArrayList<Integer> activeEmployees;
+	
+	public JMSConnector(){
+		activeEmployees = new ArrayList<Integer>();
+		
+		listenForLogin();
+		listenForRequests();
+	}
+	
+	void listenForLogin(){
+		
+	}
+	
+	public int assignOrder(Order order){
+		return -1;
+	}
+	
+	void listenForRequests(){
+		
+	}
 	
 	@Override
 	public void onMessage(Message message) {
