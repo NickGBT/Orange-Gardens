15991200c726813ec5a4c01e1e151dd5d90ef7d8
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-29 17:44:21 +0100

    MessageReceiver written. Requires a listener. - AN

---
+++ src/main/java/com/netbuilder/dops/MessageReceiver.java
@@ -0,0 +1,60 @@
+package com.netbuilder.dops;
+
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.QueueConnection;
+import javax.jms.QueueConnectionFactory;
+import javax.jms.QueueReceiver;
+import javax.jms.QueueSession;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
+
+/**
+ * 
+ * @author Alexander Neil
+ *
+ */
+public class MessageReceiver {
+
+	private Context context = null;
+	private QueueConnectionFactory connFactory = null;
+	private QueueConnection conn = null;
+	private Queue queue = null;
+	private QueueSession session = null;
+	private QueueReceiver qReceiver = null;
+	
+	public void receiverOrderAcceptance(){
+		try{
+			context = new InitialContext();
+			connFactory = (QueueConnectionFactory) context.lookup("ConnectionFactory");
+			queue = (Queue) context.lookup("orderaccept");
+			conn = connFactory.createQueueConnection();
+			session = conn.createQueueSession(false, AUTO_ACKNOWLEDGE);
+			qReceiver = session.createReceiver(queue);
+			conn.start();
+			qReceiver.setMessageListener(new AcceptanceListener()); //TODO
+		} catch (NamingException e){
+			e.printStackTrace();
+		} catch (JMSException e){
+			e.printStackTrace();
+		} finally {
+			if(context != null){
+				try{
+					context.close();
+				} catch (NamingException e){
+					e.printStackTrace();
+				}
+			}
+			if(conn != null){
+				try{
+					conn.close();
+				} catch(JMSException e){
+					conn.close();
+				}
+			}
+		}
+	}
+}
