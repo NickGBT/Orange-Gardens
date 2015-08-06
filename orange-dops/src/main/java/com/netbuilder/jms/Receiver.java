23fcf1d9889ec11088b085b574a731655df7b632
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-08-06 17:21:51 +0100

    Added logging. - AN

--- orange-dops/src/main/java/com/netbuilder/jms/Receiver.java
+++ orange-dops/src/main/java/com/netbuilder/jms/Receiver.java
@@ -10,6 +10,8 @@ import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.netbuilder.orange_dops.GladosGui;
 
@@ -23,14 +25,17 @@ import static javax.jms.Session.AUTO_ACKNOWLEDGE;
  */
 public class Receiver {
 
+	private static final Logger logger = LogManager.getLogger();
+	
 	private String broker;
 	private ActiveMQConnectionFactory connectionFactory;
 	private Connection connection;
 	
 	public Receiver(String broker) throws JMSException{
 		this.broker = broker + ":61616";
-		
+		logger.info("Using default port for broker (61616)");
 		connectionFactory = new ActiveMQConnectionFactory("tcp://" + this.broker);
+		logger.debug("Creating connection for \"tcp://" + this.broker + "\".");
 		connection = connectionFactory.createConnection();
 	}
 	
@@ -38,6 +43,7 @@ public class Receiver {
 		this.broker = broker + ":" + port;
 		
 		connectionFactory = new ActiveMQConnectionFactory("tcp://" + this.broker);
+		logger.debug("Creating connection for \"tcp://" + this.broker + "\".");
 		connection = connectionFactory.createConnection();
 	}
 	
@@ -46,6 +52,7 @@ public class Receiver {
 		try {
 			connection.start();
 		} catch (JMSException e) {
+			logger.warn("Connection error or already started", e);
 			System.err.println("Connection erroneous or already started.");
 		}
 		
@@ -56,6 +63,7 @@ public class Receiver {
 		MessageConsumer consumer = session.createConsumer(destination);
 		
 		JmsListener jmsListener = new JmsListener(source);
+		logger.debug("Setting JmsListener for queue:\"" + queue + "\"", jmsListener);
 		consumer.setMessageListener(jmsListener);
 		
 		return jmsListener;
@@ -66,6 +74,7 @@ public class Receiver {
 		try{
 			connection.start();
 		} catch (JMSException e){
+			logger.warn("Connection error or already started", e);
 			System.err.println("Connection erroneous or already started.");
 		}
 		
@@ -76,6 +85,7 @@ public class Receiver {
 		MessageConsumer consumer = session.createConsumer(destination);
 		
 		JmsListener jmsListener = new JmsListener(source);
+		logger.debug("Setting JmsListener for queue:\"" + topic + "\"", jmsListener);
 		consumer.setMessageListener(jmsListener);
 		
 		return jmsListener;
@@ -83,9 +93,11 @@ public class Receiver {
 	
 	public Object getMessage(String queue) throws JMSException{
 		
+		logger.info("Preparing to wait on a message queue");
 		try{
 			connection.start();
 		} catch(JMSException e){
+			logger.warn("Connection error or already started", e);
 			System.err.println("Connection erroneous or already started.");
 		}
 		
@@ -93,18 +105,21 @@ public class Receiver {
 		Destination destination = session.createQueue(queue);
 		MessageConsumer consumer = session.createConsumer(destination);
 		
+		logger.info("Setting timeout for blocking call to 60 seconds");
 		Message message = consumer.receive(60000);
 		Object payload;
 		
+		logger.debug("Checking type of message received");
 		if(message instanceof TextMessage){
 			payload = ((TextMessage)message).getText();
+			logger.debug("Unpacking TextMessage to payload object", payload);
 		}else if(message instanceof ObjectMessage){
 			payload = ((ObjectMessage)message).getObject();
+			logger.debug("Unpacking ObjectMessage to payload object", payload);
 		}else{
-			payload = null;
-			//PANIC!
+			logger.error("Message received is not next or object. Returning null");
+			return null;
 		}
-		
 		return payload;
 	}
 }
