d77936893dd09365345356ed51d470ea66ab1fa0
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-08-06 15:23:50 +0100

    Removed the 1

--- orange-backend/src/main/java/com/netbuilder/util/DummyAL.java
+++ orange-backend/src/main/java/com/netbuilder/util/DummyAL.java
@@ -20,7 +20,7 @@ public class DummyAL
 	
 	Product product4 = new Product("resources/img/swagShed.jpg", "Swag Shed", 1976.25, 80, 45, 18, 19.50, "Store your swag inside.", ProductCategory.Building);
 	
-	Product product5 = new Product(1, "Swag Shed", 1976.25, 80, 45, 18, 19.50, "Store your swag inside.", ProductCategory.Building);
+	Product product5 = new Product(1,"resources/img/swagShed.jpg",  "Swag Shed", 1976.25, 80, 45, 18, 19.50, "Store your swag inside.", ProductCategory.Building);
 	
 	public DummyAL(){
 		allProducts.clear();
