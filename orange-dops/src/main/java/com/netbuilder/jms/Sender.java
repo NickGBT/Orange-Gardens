445a513e14f2b0cdd47f89ac22d52841c379f5f1
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-08-06 15:36:05 +0100

    Refactored JMS classes to com.netbuilder.jms - AN

--- orange-dops/src/main/java/com/netbuilder/jms/Sender.java
+++ orange-dops/src/main/java/com/netbuilder/jms/Sender.java
@@ -1,4 +1,4 @@
-package com.netbuilder.jms.sender;
+package com.netbuilder.jms;
 
 import java.io.Serializable;
 
@@ -6,6 +6,7 @@ import javax.jms.Connection;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
@@ -20,20 +21,20 @@ public class Sender {
 	private Connection connection;
 	
 	public Sender(String broker) throws JMSException{
-		this.broker = broker;
+		this.broker = broker + ":61616";
 		
-		connectionFactory = new ActiveMQConnectionFactory("tcp://" + broker);
+		connectionFactory = new ActiveMQConnectionFactory("tcp://" + this.broker);
 		connection = connectionFactory.createConnection();
 	}
 	
 	public Sender(String broker, int port) throws JMSException{
 		this.broker = broker + ":" + port;
 		
-		connectionFactory = new ActiveMQConnectionFactory("tcp://" + broker);
+		connectionFactory = new ActiveMQConnectionFactory("tcp://" + this.broker);
 		connection = connectionFactory.createConnection();
 	}
 	
-	public void send(String payload, String destination) throws JMSException{
+	public void sendToQueue(String payload, String destination) throws JMSException{
 		
 		connection.start();
 		Session session = connection.createSession(false, AUTO_ACKNOWLEDGE);
@@ -49,7 +50,19 @@ public class Sender {
 		connection.close();
 	}
 	
-	public void send(Serializable payload, String destination){
+	public void sendToQueue(Serializable payload, String destination) throws JMSException{
 		
+		connection.start();
+		Session session = connection.createSession(false, AUTO_ACKNOWLEDGE);
+		
+		Destination dest = session.createQueue(destination);
+		MessageProducer producer = session.createProducer(dest);
+		
+		ObjectMessage message = session.createObjectMessage(payload);
+		
+		producer.send(message);
+		
+		session.close();
+		connection.close();
 	}
 }
