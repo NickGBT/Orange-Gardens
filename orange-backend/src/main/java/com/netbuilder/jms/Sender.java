42175ea955d972677288953f24adb04aadc99b2c
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-08-07 13:37:34 +0100

    Preparing injection of connections. - AN

--- orange-backend/src/main/java/com/netbuilder/jms/Sender.java
+++ orange-backend/src/main/java/com/netbuilder/jms/Sender.java
@@ -1,5 +1,15 @@
 package com.netbuilder.jms;
 
+import javax.annotation.ManagedBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jms.Connection;
+
+@Stateless
+@ManagedBean(value="Sender")
 public class Sender {
 
+	@Inject
+	private Connection connection;
+	
 }
