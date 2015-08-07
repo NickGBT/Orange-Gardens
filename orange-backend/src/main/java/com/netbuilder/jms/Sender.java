4606d8e658b7debe4ea3bdf9b775f9edaaa0b9ab
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-08-07 15:33:16 +0100

    Added code to inject a connection factory and send a message. (untested). - AN

--- orange-backend/src/main/java/com/netbuilder/jms/Sender.java
+++ orange-backend/src/main/java/com/netbuilder/jms/Sender.java
@@ -2,15 +2,52 @@ package com.netbuilder.jms;
 
 import javax.annotation.ManagedBean;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import static javax.jms.Session.AUTO_ACKNOWLEDGE;
 
 @Stateless
 @ManagedBean(value="Sender")
 public class Sender {
 
-	@Inject
-	private Connection connection;
+	public void sentToQueue(String payload, String destinationQueue){
+		
+	Context context;
+	try {
+		context = new InitialContext();
+		ActiveMQConnectionFactory connectionFactory = (ActiveMQConnectionFactory) context.lookup("java:jboss/exported/ConnectionFactory");
+		
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false, AUTO_ACKNOWLEDGE);
+		
+		Destination destination = session.createQueue(destinationQueue);
+		MessageProducer producer = session.createProducer(destination);
+		
+		TextMessage message = session.createTextMessage(payload);
+		
+		producer.send(message);
+		
+		session.close();
+		connection.close();
+		
+	} catch (NamingException e) {
+		e.printStackTrace();
+	} catch (JMSException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
-
+	}
 }
