791695070733d756c3f53a951ad191c613c76cd9
luklew <llew@PC-CZC046BFPL.netbuilder.private>
2015-08-10 11:52:12 +0100

    added IMS to repo

---
+++ ims/src/main/java/com/netbuilder/ims/main/ApplicationLoader.java
@@ -0,0 +1,24 @@
+package com.netbuilder.ims.main;
+
+import com.netbuilder.ims.controller.StockManagerController;
+import com.netbuilder.ims.model.StockManager;
+import com.netbuilder.ims.view.StockManagerFrame;
+
+
+/*
+ *Main class sets up the view and model, then sends the references to 
+ *the controller.
+ */
+public class ApplicationLoader {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		StockManagerFrame view = new StockManagerFrame();
+		
+		StockManager model = new StockManager();
+		
+		StockManagerController controller = new StockManagerController(model, view);
+
+	}
+
+}
---
+++ ims/src/main/java/com/netbuilder/ims/view/OrderStockFrame.java
@@ -0,0 +1,32 @@
+package com.netbuilder.ims.view;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/*
+ * This class creates a new window which displays 
+ * a table which contains products that need to be ordered.
+ */
+
+public class OrderStockFrame extends JFrame{
+	
+	private OrderStockPanel orderStock;
+	
+	public OrderStockFrame(){
+		this.setTitle("Order Stock");
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setPreferredSize(new Dimension(450,600));
+		//this.setResizable(false);
+	
+		orderStock = new OrderStockPanel();
+		this.add(orderStock);
+	
+		this.pack();
+		this.setVisible(true);
+	}
+
+	public OrderStockPanel getOrderPanel(){
+		return orderStock;
+	}
+}
