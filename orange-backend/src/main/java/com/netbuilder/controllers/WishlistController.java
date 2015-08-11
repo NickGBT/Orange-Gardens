c0a8fe327297bebf90d0aa92a9644506d6722825
Jordan Taylor <taylorcj@live.co.uk>
2015-08-11 13:04:44 +0100

    User can view their wishlist -JT

--- orange-backend/src/main/java/com/netbuilder/controllers/WishlistController.java
+++ orange-backend/src/main/java/com/netbuilder/controllers/WishlistController.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import com.netbuilder.entities.LoginDetails;
 import com.netbuilder.entities.OrderLine;
+import com.netbuilder.entities.Product;
 import com.netbuilder.entity_managers.interfaces.LoginDetailsManager;
 import com.netbuilder.entity_managers.interfaces.OrderLineManager;
 import com.netbuilder.entity_managers.interfaces.OrderManager;
 import com.netbuilder.entity_managers.interfaces.ProductManager;
+import com.netbuilder.enums.OrderStatus;
 import com.netbuilder.util.OrderDetails;
 import com.netbuilder.util.UserId;
 
 /**
  * 
- * @author ngilbert & jtaylor
+ * @author jtaylor
  *
  */
 
@@ -28,6 +32,19 @@ public class WishlistController
 	private UserId userId;
 	@Inject
 	private OrderLineManager orderLineManager;
+	@Inject
+	private ProductManager pm;
+	@Inject
+	private LoginDetailsManager ldm;
+	@Inject
+	private OrderLineManager olm;
+	@Inject
+	private OrderManager om;
+
+	private String productId;
+	private LoginDetails loginDet;
+	private Product foundProduct;
+	private OrderLine orderLine;
 
 	private List<OrderLine> wishlist;
 
@@ -39,6 +56,34 @@ public class WishlistController
 
 	public void setWishlist(List<OrderLine> wishlist) {
 		this.wishlist = wishlist;
+	}
+	
+
+	public void removeFromWishlist() 
+	{
+		productId = FacesContext.getCurrentInstance().getExternalContext()
+				.getRequestParameterMap().get("productId");
+
+		foundProduct = pm.findByProductId(Integer.parseInt(productId));
+
+		loginDet = ldm.findByUsername(userId.getUsername());
+
+		if (om.findBasketByUsername(OrderStatus.wishlist, userId.getUsername()) != null) 
+		{
+			if (olm.findByProductInWishlist(foundProduct.getProductId()) != null) 
+			{
+				orderLine = olm.findByProductId(foundProduct.getProductId());
+				olm.removeProductLine(orderLine);
+			} 
+			else 
+			{
+				System.out.println("WishlistController::Line80:: Wishlist does not contain said item.");
+			}
+		} 
+		else 
+		{
+			System.out.println("WishlistController::Line85:: Cannot find user wishlist.");
+		}
 	}
 
 
