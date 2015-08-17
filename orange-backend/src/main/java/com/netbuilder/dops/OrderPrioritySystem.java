e98fd92d927b2ea9bfad610b0f5fe4073ce96e3b
JustinMabbutt <Justin.Mabbutt@netbuilder.com>
2015-08-17 10:57:46 +0100

    Milliseconds date conversion works - JJM

--- orange-backend/src/main/java/com/netbuilder/dops/OrderPrioritySystem.java
+++ orange-backend/src/main/java/com/netbuilder/dops/OrderPrioritySystem.java
@@ -1,9 +1,16 @@
 package com.netbuilder.dops;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.netbuilder.entities.Order;
 import com.netbuilder.entity_managers.interfaces.OrderManager;
@@ -18,10 +25,14 @@ import com.netbuilder.enums.OrderStatus;
 public class OrderPrioritySystem {
 
 	private OrderManager orders;
+	private long milliseconds;
 	private long currentTime = System.currentTimeMillis();
+	private SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+	private Date datePlaced;
+	private Calendar calendar;
+	private static final Logger logger = LogManager.getLogger();
 
-	private ArrayList<Order> placedOrders = (ArrayList<Order>) orders
-			.findByStatus(OrderStatus.placed);
+	private ArrayList<Order> placedOrders = (ArrayList<Order>) orders.findByStatus(OrderStatus.placed);
 
 	/**
 	 * This method generates a prioritised queue sorted in blocks of priorities
@@ -29,21 +40,37 @@ public class OrderPrioritySystem {
 	 * priority (critical) to lowest
 	 * 
 	 **/
+	
+	public long changeDateToMillis(String date)
+	{
+		try
+		{
+			datePlaced = dateFormatter.parse(date);
+		} 
+		catch (ParseException pe) 
+		{
+			logger.error("Date format incorrect", date);
+			pe.printStackTrace();
+		}
+		calendar.setTime(datePlaced);
+		milliseconds = calendar.getTimeInMillis();
+		return milliseconds;
+	}
 
 	public List<Order> getPrioritisedQueue() {
 		ArrayList<Order> lowPriority = new ArrayList<Order>();
 
 		for (Order o : placedOrders) {
-			if ((currentTime - o.getDatePlacedInMillis()) < 360000) {
+			if ((currentTime - changeDateToMillis(o.getDatePlaced())) < 360000) {
 				lowPriority.add(o);
 			}
 		}
 
 		Collections.sort(lowPriority, new Comparator<Order>() {
 			public int compare(Order o1, Order o2) {
-				if (o1.getDatePlacedInMillis() < o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) < changeDateToMillis(o2.getDatePlaced()))
 					return 1;
-				if (o1.getDatePlacedInMillis() > o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) > changeDateToMillis(o2.getDatePlaced()))
 					return -1;
 				return 0;
 			}
@@ -52,8 +79,8 @@ public class OrderPrioritySystem {
 		ArrayList<Order> mediumPriority = new ArrayList<Order>();
 
 		for (Order o : placedOrders) {
-			if ((currentTime - o.getDatePlacedInMillis()) < (360000 * 6)
-					|| (currentTime - o.getDatePlacedInMillis()) > (360000)) {
+			if ((currentTime - changeDateToMillis(o.getDatePlaced())) < (360000 * 6)
+					|| (currentTime - changeDateToMillis(o.getDatePlaced())) > (360000)) {
 				mediumPriority.add(o);
 			}
 		}
@@ -61,9 +88,9 @@ public class OrderPrioritySystem {
 		Collections.sort(mediumPriority, new Comparator<Order>() {
 			@Override
 			public int compare(Order o1, Order o2) {
-				if (o1.getDatePlacedInMillis() < o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) < changeDateToMillis(o2.getDatePlaced()))
 					return 1;
-				if (o1.getDatePlacedInMillis() > o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) > changeDateToMillis(o2.getDatePlaced()))
 					return -1;
 				return 0;
 			}
@@ -72,8 +99,8 @@ public class OrderPrioritySystem {
 		ArrayList<Order> highPriority = new ArrayList<Order>();
 
 		for (Order o : placedOrders) {
-			if ((currentTime - o.getDatePlacedInMillis()) < (360000 * 24)
-					|| (currentTime - o.getDatePlacedInMillis()) > (360000 * 6)) {
+			if ((currentTime - changeDateToMillis(o.getDatePlaced())) < (360000 * 24)
+					|| (currentTime - changeDateToMillis(o.getDatePlaced())) > (360000 * 6)) {
 				highPriority.add(o);
 			}
 		}
@@ -81,9 +108,9 @@ public class OrderPrioritySystem {
 		Collections.sort(highPriority, new Comparator<Order>() {
 			@Override
 			public int compare(Order o1, Order o2) {
-				if (o1.getDatePlacedInMillis() < o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) < changeDateToMillis(o2.getDatePlaced()))
 					return 1;
-				if (o1.getDatePlacedInMillis() > o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) > changeDateToMillis(o2.getDatePlaced()))
 					return -1;
 				return 0;
 			}
@@ -92,7 +119,7 @@ public class OrderPrioritySystem {
 		ArrayList<Order> criticalPriority = new ArrayList<Order>();
 
 		for (Order o : placedOrders) {
-			if ((currentTime - o.getDatePlacedInMillis()) < (360000 * 36)) {
+			if ((currentTime - changeDateToMillis(o.getDatePlaced())) < (360000 * 36)) {
 				criticalPriority.add(o);
 			}
 		}
@@ -100,9 +127,9 @@ public class OrderPrioritySystem {
 		Collections.sort(criticalPriority, new Comparator<Order>() {
 			@Override
 			public int compare(Order o1, Order o2) {
-				if (o1.getDatePlacedInMillis() < o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) < changeDateToMillis(o2.getDatePlaced()))
 					return 1;
-				if (o1.getDatePlacedInMillis() > o2.getDatePlacedInMillis())
+				if (changeDateToMillis(o1.getDatePlaced()) > changeDateToMillis(o2.getDatePlaced()))
 					return -1;
 				return 0;
 			}
