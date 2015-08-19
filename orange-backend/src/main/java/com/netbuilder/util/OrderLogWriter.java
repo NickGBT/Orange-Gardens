bb4747733fed0a6337c417ecd35c8555a8a7d7a7
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-08-19 13:49:37 +0100

    Stub for order logging. -AN

---
+++ orange-backend/src/main/java/com/netbuilder/util/OrderLogWriter.java
@@ -0,0 +1,16 @@
+package com.netbuilder.util;
+
+import com.netbuilder.entities.Order;
+
+public class OrderLogWriter {
+
+	public static String logOrder(Order order){
+		
+		
+		return null;
+	}
+	
+	public static String logLine(OrderLine oLine){
+		
+	}
+}
