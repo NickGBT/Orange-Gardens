38efcd0ecc01795cbf298b79d7d8a0cfb68cb5d9
JustinMabbutt <Justin.Mabbutt@netbuilder.com>
2015-08-20 10:52:37 +0100

    fixed server error - JJM

--- orange-backend/src/main/java/com/netbuilder/util/IsLoggedIn.java
+++ orange-backend/src/main/java/com/netbuilder/util/IsLoggedIn.java
@@ -1,13 +1,21 @@
 package com.netbuilder.util;
 
+import java.io.Serializable;
+
 //import javax.faces.bean.SessionScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 
+/**
+ * 
+ * @author ???????
+ *
+ */
 @ManagedBean(name = "isLoggedIn")
 @SessionScoped
-public class IsLoggedIn {
+public class IsLoggedIn implements Serializable {
 
+	private static final long serialVersionUID = 5440963945386351267L;
 	private boolean isLoggedIn = false;
 	private int userId;
 	
@@ -33,5 +41,4 @@ public class IsLoggedIn {
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-
 }
